5da37dd939105f62c562d3ad865d3b23f279f2ae
NITESH KALANGADA <153585475+niteshkal03@users.noreply.github.com>
2026-07-10 06:43:22 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,6 +3,6 @@ echo "User : $(whoami)"
 echo "Hostname : $(hostname)"
 echo "Current Directory : $(pwd)"
 echo "Date : $(date)"
-sudo apt update
+sudo apt update -y
 sudo apt install -y nginx
 nginx --version
